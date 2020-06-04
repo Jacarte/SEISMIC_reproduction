c5c3371ba31108de3261e66eb5d809c82fcffc01
Orestis Floros <forestis@kth.se>
2020-06-04 09:27:48 +0200

    Reformat train.py

--- train.py
+++ train.py
@@ -1,45 +1,41 @@
-
+#!/usr/bin/env python3
+import joblib
 import numpy as np
-from sklearn import svm, datasets
+from sklearn import datasets, svm
 from sklearn.model_selection import cross_val_score
 
+# Miners
 from data import astart as astar
 from data import asteroids as asteroids
-from data import funkykarts as funky
-from data import creature_pack as cpack
-from data import bullets  as bullets
-from data import basic4gl  as basic4gl
-from data import tanks  as tanks
-
-# Miners
-from data import yazec_original as yazec
+from data import basic4gl as basic4gl
+from data import bullets as bullets
 from data import coinhive as coinhive
+from data import creature_pack as cpack
 from data import cryptonight as cryptonight
+from data import funkykarts as funky
+from data import tanks as tanks
+from data import yazec_original as yazec
 
 noMiners = astar + asteroids + funky + cpack + bullets + tanks + basic4gl
 miners = yazec + coinhive + cryptonight.tolist()
 
 X = np.array(noMiners + miners)
-y = np.array([0]*len(noMiners) + [1]*len(miners))
+y = np.array([0] * len(noMiners) + [1] * len(miners))
 
 # we create an instance of SVM and fit out data. We do not scale our
 # data since we want to plot the support vectors
 C = 10.0  # SVM regularization parameter
-model = svm.SVC(kernel='linear', C=C)
+model = svm.SVC(kernel="linear", C=C)
 model = model.fit(X, y)
 
-import joblib
-
-joblib.dump(model,"svm.pkl")
+joblib.dump(model, "svm.pkl")
 
 for score in ["accuracy ", "precision", "recall   "]:
     print(score, end=" ")
-    
+
     values = cross_val_score(model, X, y, cv=10, scoring=score.replace(" ", ""))
     mean = values.mean()
 
-    print(" (mean %.5f)"%mean, end= " ")
+    print(" (mean %.5f)" % mean, end=" ")
     print(" : ", end=" ")
-    print(" ".join("%.7f "%v for v in values))
-
-
+    print(" ".join("%.7f " % v for v in values))
