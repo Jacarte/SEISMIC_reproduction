cd91546ca7f76bac062d42da73c8257a95c7531a
Orestis Floros <forestis@kth.se>
2020-06-04 09:30:41 +0200

    Split to train & test

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 import joblib
 import numpy as np
 from sklearn import datasets, svm
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 
 # Miners
 from data import astart as astar
@@ -21,19 +21,24 @@ miners = yazec + coinhive + cryptonight.tolist()
 
 X = np.array(noMiners + miners)
 y = np.array([0] * len(noMiners) + [1] * len(miners))
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # we create an instance of SVM and fit out data. We do not scale our
 # data since we want to plot the support vectors
 C = 10.0  # SVM regularization parameter
 model = svm.SVC(kernel="linear", C=C)
-model = model.fit(X, y)
+model = model.fit(X_train, y_train)
 
 joblib.dump(model, "svm.pkl")
 
 for score in ["accuracy ", "precision", "recall   "]:
     print(score, end=" ")
 
-    values = cross_val_score(model, X, y, cv=10, scoring=score.replace(" ", ""))
+    values = cross_val_score(
+        model, X_test, y_test, cv=10, scoring=score.strip()
+    )
     mean = values.mean()
 
     print(" (mean %.5f)" % mean, end=" ")
